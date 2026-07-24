6de4710bb3ca111e82423a3546a232ebeba75e5d
Andrey Krepyshev <krep83@gmail.com>
2026-07-24 12:01:57 +0300

    grind

---
+++ src/engine/defs/jump_point.ts
@@ -0,0 +1,17 @@
+// src/engine/defs/jump_point.ts
+
+export const JUMP_POINT_OBJECT_SPRITE_ID = {
+    JUMP_POINT_00: 'jump_point_00',
+} as const;
+
+export type JumpPointObjectSpriteId = (typeof JUMP_POINT_OBJECT_SPRITE_ID)[keyof typeof JUMP_POINT_OBJECT_SPRITE_ID];
+
+export type JumpPointState = {
+    id: string;
+    name: string;
+
+    // Нода, для прыжка в которую рассчитана эта точка.
+    targetNodeId: string;
+
+    objectSpriteId: JumpPointObjectSpriteId;
+};
